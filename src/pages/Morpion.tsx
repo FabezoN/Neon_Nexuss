66cb97c6b427c689547bbede42f2eff8fdf12b14
dorianalbertlaik <dorian.albert1@ynov.com>
2024-02-12 09:48:48 +0100

    ajout de la partie Morpion

--- src/pages/Morpion.tsx
+++ src/pages/Morpion.tsx
@@ -1,10 +1,11 @@
 import Layout from "../components/Layout/Layout";
+import {TicTacToe} from "../components/tic-tac-toe/tic-tac-toe.tsx";
 
 const Morpion: React.FC = () => (
     <div className="Morpion">
         <Layout>
         </Layout>
-        <h1>Morpion</h1>
+        <TicTacToe/>
     </div>
 
 );
